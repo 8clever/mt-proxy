adb66f05a0bdd8500d99ad9d05681a6ab2fb3b27
Ivan Vitiaev <godofluck89@gmail.com>
2026-08-08 16:03:37 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: "/mt-proxy/"
+    base: "/mt-proxy/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
